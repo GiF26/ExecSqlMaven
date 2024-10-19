b94fd1a7737bc974b5504e462acd5fbe98ece59b
Giovana Ferreira <giovannapink26@gmail.com>
2024-10-19 17:41:39 -0300

    arrumar status da label de importação

--- src/main/java/ExecutarSQL.java
+++ src/main/java/ExecutarSQL.java
@@ -1,6 +1,16 @@
+import org.mozilla.universalchardet.UniversalDetector;
+
+import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.List;
 
 public class ExecutarSQL {
 
@@ -10,6 +20,78 @@ public class ExecutarSQL {
     public ExecutarSQL(Connection conexao) {
         this.conexao = conexao;
     }
+
+    // Método para permitir ao usuário selecionar o diretório
+    String selecionarDiretorioEExecutarSQL() {
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+        String caminho = "";
+
+        int returnValue = fileChooser.showOpenDialog(null);
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+            File selectedDirectory = fileChooser.getSelectedFile();
+            caminho = String.valueOf(fileChooser.getSelectedFile());
+            System.out.println("Executando arquivos do diretório...");
+//            lblInfo.setText("Executando arquivos do diretório...");
+//            executarSQLDoDiretorio(selectedDirectory);
+        } else {
+            System.out.println("Nenhum diretório selecionado");
+//            lblInfo.setText("Nenhum diretório selecionado.");
+        }
+
+        return caminho;
+    }
+
+    // Método para listar e executar todos os arquivos SQL no diretório
+    void executarSQLDoDiretorio(File diretorio) {
+        try {
+
+            File[] arquivos = diretorio.listFiles((dir, name) -> name.toLowerCase().endsWith(".sql"));
+            if (arquivos != null && arquivos.length > 0) {
+                for (File arquivoSQL : arquivos) {
+                    String sql = lerConteudoDoArquivo(arquivoSQL.toPath());
+                    try {
+                        System.out.println("Arquivo " + arquivoSQL.getName() + " executado com sucesso.\n");
+                        executarComando(sql);
+//                        lblInfo.setText("Arquivo " + arquivoSQL.getName() + " executado com sucesso.\n");
+//                        lblInfo.getText();
+                    } catch (SQLException ex) {
+                        System.out.println("Erro ao executar " + arquivoSQL.getName() + ": " + ex.getMessage() + "\n");
+//                        lblInfo.setText("Erro ao executar " + arquivoSQL.getName() + ": " + ex.getMessage() + "\n");
+//                        lblInfo.getText();
+                    }
+                }
+                System.out.println("Execução completa.");
+//                lblInfo.setText("Execução completa.");
+//                lblInfo.getText();
+            } else {
+//                lblInfo.setText("Nenhum arquivo .sql encontrado no diretório.");
+//                lblInfo.getText();
+                System.out.println("Nenhum arquivo .sql encontrado no diretório.");
+            }
+        } catch (Exception ex) {
+//            lblInfo.setText("Erro ao executar SQL: " + ex.getMessage() + "\n");
+//            lblInfo.getText();
+//            lblInfo.setText("Erro ao executar SQL.");
+//            lblInfo.getText();
+        }
+    }
+
+    private String lerConteudoDoArquivo(Path caminhoDoArquivo) throws IOException {
+        byte[] bytes = Files.readAllBytes(caminhoDoArquivo);
+        UniversalDetector detector = new UniversalDetector(null);
+        detector.handleData(bytes, 0, bytes.length);
+        detector.dataEnd();
+        String encoding = detector.getDetectedCharset();
+        detector.reset();
+
+        // Se o charset não for detectado, use UTF-8 por padrão
+        Charset charset = (encoding != null) ? Charset.forName(encoding) : StandardCharsets.UTF_8;
+        List<String> linhas = Files.readAllLines(caminhoDoArquivo, charset);
+        return String.join("\n", linhas);
+    }
+
 
     // Método para executar um comando SQL
     public void executarComando(String sql) throws SQLException, SQLException {
--- src/main/java/mainScreenTest.java
+++ src/main/java/mainScreenTest.java
@@ -2,12 +2,8 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 
 public class mainScreenTest extends JFrame {
     private JLabel llbTitle;
@@ -34,7 +30,8 @@ public class mainScreenTest extends JFrame {
     private String user;
     private String password;
     private Connection conexao;
-    private String caminho;
+    private ExecutarSQL exec = new ExecutarSQL(conexao);
+//    private String caminho;
 
     // Método principal
     public static void main(String[] args) {
@@ -57,8 +54,7 @@ public class mainScreenTest extends JFrame {
         btnPath.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                selecionarDiretorioEExecutarSQL();
-                txtPath.setText(caminho);
+                txtPath.setText(exec.selecionarDiretorioEExecutarSQL());
                 txtPath.getText();
             }
         });
@@ -76,67 +72,12 @@ public class mainScreenTest extends JFrame {
                 try {
                     ConectionConf conf = new ConectionConf(server, dataBase, user, password, port);
                     conexao = conf.conectar(server, dataBase, user, password, port);
-                    executarSQLDoDiretorio(new File(txtPath.getText()));
+                    exec = new ExecutarSQL(conexao);
+                    exec.executarSQLDoDiretorio(new File(txtPath.getText()));
                 } catch (SQLException ex) {
                     throw new RuntimeException(ex);
                 }
             }
         });
     }
-
-    // Método para permitir ao usuário selecionar o diretório
-    private void selecionarDiretorioEExecutarSQL() {
-        JFileChooser fileChooser = new JFileChooser();
-        fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-        int returnValue = fileChooser.showOpenDialog(null);
-        if (returnValue == JFileChooser.APPROVE_OPTION) {
-            File selectedDirectory = fileChooser.getSelectedFile();
-            caminho = String.valueOf(fileChooser.getSelectedFile());
-            lblInfo.setText("Executando arquivos do diretório...");
-//            executarSQLDoDiretorio(selectedDirectory);
-        } else {
-            lblInfo.setText("Nenhum diretório selecionado.");
-        }
-    }
-
-    // Método para listar e executar todos os arquivos SQL no diretório
-    private void executarSQLDoDiretorio(File diretorio) {
-        try {
-            ExecutarSQL executorSQL = new ExecutarSQL(conexao);
-
-            File[] arquivos = diretorio.listFiles((dir, name) -> name.toLowerCase().endsWith(".sql"));
-            if (arquivos != null && arquivos.length > 0) {
-                for (File arquivoSQL : arquivos) {
-                    String sql = lerConteudoDoArquivo(arquivoSQL.toPath());
-                    try {
-                        executorSQL.executarComando(sql);
-                        lblInfo.setText("Arquivo " + arquivoSQL.getName() + " executado com sucesso.\n");
-                        lblInfo.getText();
-                    } catch (SQLException ex) {
-                        lblInfo.setText("Erro ao executar " + arquivoSQL.getName() + ": " + ex.getMessage() + "\n");
-                        lblInfo.getText();
-                    }
-                }
-                lblInfo.setText("Execução completa.");
-                lblInfo.getText();
-            } else {
-                lblInfo.setText("Nenhum arquivo .sql encontrado no diretório.");
-                lblInfo.getText();
-            }
-        } catch (Exception ex) {
-            lblInfo.setText("Erro ao executar SQL: " + ex.getMessage() + "\n");
-            lblInfo.getText();
-            lblInfo.setText("Erro ao executar SQL.");
-            lblInfo.getText();
-        }
-    }
-
-    // Método para ler o conteúdo de um arquivo SQL
-    private String lerConteudoDoArquivo(Path caminhoDoArquivo) throws IOException {
-        List<String> linhas = Files.readAllLines(caminhoDoArquivo);
-        System.out.println("leu");
-        return String.join("\n", linhas);
-    }
-
 }
